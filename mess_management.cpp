651d1674ac673a1a269b1326174cfa2bfdfb0181
Yashirathore11 <47051790+Yashirathore11@users.noreply.github.com>
2020-05-24 15:23:03 +0530

    Create mess_management.cpp

---
+++ mess_management.cpp
@@ -0,0 +1,446 @@
+#include<iostream>
+#include<unistd.h>
+#include<conio.h>
+#include<string.h>
+#include<stdio.h>
+#include<process.h>
+#include<iomanip>
+#include<dos.h>
+#include<graphics.h>
+using namespace std;
+main()
+ {
+ // clrscr();
+  unsigned int sum,add,d,i,j,e,f,g,h,k,l,w,x,y,z,me;
+  unsigned int bir=10,per=36,der=110,mur=8,par=110,cor=90,twr=1199, lur=17,tir=107,
+	       rer=1499;
+  int qubis=1000,abis=0,qupep=1000,apep=0,quden=110,aden=0,qumun=1000,amun=0,quperk=500,aperk=0,qucoc=400,acoc=0,qutit=100,atit=0,qulux=1000,alux=0,qutid=1000,atid=0,quree=300,aree=0;
+  unsigned int m=0,n=0,o=0,p=0,q=0,r=0,s=0,t=0,u=0,v=0;
+  char name[50],rank[10],pu,c[20],a[15],b[15],ok;
+ // clrscr();
+ printf("\n +++++++++++++++++++++++++++++++++++++++*++++++++++++++++++++++++++++++++++++++");
+ printf("\n +                                     ***                                    +");
+ printf("\n +                                    *****                                   +");
+ printf("\n +                                   *******                                  +");
+ printf("\n +                                  *********                                 +");
+ printf("\n +                                 ***********                                +");
+ printf("\n +                                *************                               +");
+ printf("\n +                               ***************                              +");
+ printf("\n +                              *****************                             +");
+ printf("\n +                             *******************                            +");
+ printf("\n +                            *********************                           +");
+ printf("\n +                           ***********************                          +");
+ printf("\n +                          *************************                         +");
+ printf("\n +                         ***************************                        +");
+ printf("\n +                        *****************************                       +");
+ printf("\n +                       *******************************                      +");
+ printf("\n +                      *********************************                     +");
+ printf("\n +                     ***********************************                    +");
+ printf("\n +                    *************************************                   +");
+ printf("\n +                   ***************************************                  +");
+ printf("\n +                  *****************************************                 +");
+ printf("\n +                 *******************************************                +");
+ printf("\n +                *********************************************               +");
+ printf("\n +               ***********************************************              +");
+ printf("\n +              *************************************************             +");
+ printf("\n +             *********************WELCOME***********************            +");
+ printf("\n +              ***********************TO************************             +");
+ printf("\n +               *******************ERRORYS3********************              +");
+ printf("\n +                ********************MESS*********************               +");
+ printf("\n +                 *******************************************                +");
+ printf("\n +                   ***************************************                  +");
+ printf("\n +                    *************************************                   +");
+ printf("\n +                     ***********************************                    +");
+ printf("\n +                      *********************************                     +");
+ printf("\n +                       *******************************                      +");
+ printf("\n +                        *****************************                       +");
+ printf("\n +                         ***************************                        +");
+ printf("\n +                          *************************                         +");
+ printf("\n +                           ***********************                          +");
+ printf("\n +                            *********************                           +");
+ printf("\n +                             *******************                            +");
+ printf("\n +                              *****************                             +");
+ printf("\n +                               ***************                              +");
+ printf("\n +                                *************                               +");
+ printf("\n +                                 ***********                                +");
+ printf("\n +                                  *********                                 +");
+ printf("\n +                                   *******                                  +");
+ printf("\n +                                    *****                                   +");
+ printf("\n +                                     ***                                    +");
+ printf("\n +++++++++++++++++++++++++++++++++++++++*++++++++++++++++++++++++++++++++++++++");
+  getch();
+ // clrscr();
+  cout<<"\n\n\t      w             w    e e e e e e e e    l            ";
+  cout<<"\n\n\t      w      w      w    e                  l            ";
+  cout<<"\n\n\t      w     w w     w    e                  l            ";
+  cout<<"\n\n\t      w    w   w    w    e e e e e e e e    l            ";
+  cout<<"\n\n\t      w   w     w   w    e e e e e e e e    l            ";
+  cout<<"\n\n\t      w  w       w  w    e                  l            ";
+  cout<<"\n\n\t      w w         w w    e                  l            ";
+  cout<<"\n\n\t      w             w    e e e e e e e e    l l l l l l l l";
+  cout<<"\n\n\n\n   c c c c c c c      o o o o o o o     m               m    e e e e e e e e   ";
+  cout<<"\n\n  c                  o             o    m m           m m    e  ";
+  cout<<"\n\n  c                  o             o    m  m         m  m    e     ";
+  cout<<"\n\n  c                  o             o    m   m       m   m    e e e e e e e e      ";
+  cout<<"\n\n  c                  o             o    m    m     m    m    e e e e e e e e    ";
+  cout<<"\n\n  c                  o             o    m     m   m     m    e      ";
+  cout<<"\n\n  c                  o             o    m      m m      m    e     ";
+  cout<<"\n\n   c c c c c c c      o o o o o o o     m       m       m    e e e e e e e e    ";
+  getch();
+  //clrscr();
+  main:;
+  cout<<"\n\n\n\t\t\t     ERRORYS3 MESS MANAGEMENT";
+      cout<<"\n\n\nCONSUMER INFORMATION";
+      cout<<"\n\n\n\n NAME:";
+      cin>>name;
+
+      cout<<"\n SERVICE NO.:";
+      cin>>a;
+      cout<<"\n PHONE NO.:";
+      cin>>c;
+      cout<<"\n ENTER BILL NO.:";
+      cin>>i;
+      again:;
+      
+      cout<<"\n\n\n\n 1. MENU & PURCHASE";
+      cout<<"\n\n\n 2. IMPORTANT INSTRUCTION";
+      cout<<"\n\n\n 3. NEW CUSTOMER";
+      cout<<"\n\n\n 4. QUANTITY AVAILABLE";
+      cout<<"\n\n\n 5. EXIT";
+      cout<<"\n\n\nENTER YOUR CHOICE:";
+      cin>>d;
+     
+  switch(d)
+  {
+     case 1:
+      cout<<"\nITEMS\t\t\t\t\t\t\t RATE(RS)";
+      cout<<"\n\n\nManchurian\t\t\t\t\t\t70  ";
+      cout<<"\n\nPani puri\t\t\t\t\t\t20";
+      cout<<"\n\nFried maggi\t\t\t\t\t\t100   ";
+      cout<<"\n\nVeg cheese sandwich\t\t\t\t\t80  ";
+      cout<<"\n\nPav bhaji\t\t\t\t\t\t100   ";
+      cout<<"\n\nBhel puri\t\t\t\t\t\t30  ";
+      cout<<"\n\nSamosa\t\t\t\t\t\t\t10   ";
+      cout<<"\n\nMix fruit shake\t\t\t\t\t\t50   ";
+      cout<<"\n\nMisel pav\t\t\t\t\t\t30   ";
+      cout<<"\n\nchana dal vada\t\t\t\t\t\t39   ";
+      start:
+      cout<<"\n\n\nDO YOU WANT TO PURCHASE(Y/N):\n";
+      cin>>pu;
+	if(pu=='Y'||pu=='y')
+	{switch(pu)
+	 case 1:
+	  //clrscr();
+	  cout<<"\n\n\n\t\t\t\tPURCHASE LIST";
+	  bis:;
+	  cout<<"\nManchurian (MAX 30): ";
+	  cin>>m;
+ 
+	  if(m>30)
+	  {
+	   cout<<"Enter Quantity Smaller than 30";
+	  getch();
+	  goto bis;
+	  }
+	  else
+	  abis=qubis-m;
+	  pep:;
+	  cout<<"\nPani puri(MAX 2): ";
+	  cin>>n;
+	   if(n>2)
+	  {
+	   cout<<"Enter Quantity Smaller than 2";
+	  getch();
+	  goto pep;
+	  }
+	  else
+	  apep=qupep-n;
+	  den:;
+	  cout<<"\nFried maggi(MAX 2): ";
+	  cin>>o;
+	  if(o>2)
+	  {
+	   cout<<"Enter Quantity Smaller then 2";
+	  getch();
+	  goto den;
+	  }
+	  else
+	  aden=quden-o;
+	  mun:;
+	  cout<<"\n Veg cheese sandwich(MAX 50): ";
+	  cin>>p;
+ 
+	  if(p>50)
+	  {
+	   cout<<"Enter Quantity Smaller then 50";
+	  getch();
+	  goto mun;
+	  }
+	  else
+	  amun=qumun-p;
+	  park:;
+	  cout<<"\n pav bhaji(MAX 2): ";
+	  cin>>q;
+ 
+	  if(q>2)
+	  {
+	   cout<<"Enter Quantity Smaller then 2";
+	  getch();
+	  goto park;
+	  }
+	  else
+	  aperk=quperk-q;
+	  coco:;
+	  cout<<"\n Bhel puri(MAX 2): ";
+	  cin>>r;
+ 
+	  if(r>2)
+	  {
+	   cout<<"Enter Quantity Smaller then 2";
+	  getch();
+	  goto coco;
+	  }
+	  else
+	  acoc=qucoc-r;
+	  titan:;
+	  cout<<"\n Samosa(MAX 2): ";
+	  cin>>s;
+ 
+	  if(s>2)
+	  {
+	   cout<<"Enter Quantity Smaller then 2";
+	  getch();
+ 
+	  goto titan;
+	  }
+	  else
+	  atit=qutit-s;
+	  lux:;
+	  cout<<"\nnMix fruit shake(MAX 10): ";
+	  cin>>t;
+ 
+	  if(t>10)
+	  {
+	   cout<<"Enter Quantity Smaller then 10";
+	  getch();
+	  goto lux;
+	  }
+	  else
+	  alux=qulux-t;
+	  tid:;
+	  cout<<"\n Misel pav(MAX 2): ";
+	  cin>>u;
+ 
+	  if(u>2)
+	  {
+	   cout<<"Enter Quantity Smaller then 2";
+	  getch();
+	  goto tid;
+	  }
+	  else
+	  atid=qutid-u;
+	  ree:;
+	  cout<<"\n Chana dal vada(MAX 4): ";
+	  cin>>v;
+ 
+	  if(v>4)
+	  {
+	   cout<<"Enter Quantity Smaller then 4";
+	  getch();
+	  goto ree;
+	  }
+	  else
+	  aree=quree-v;
+	  cout<<"\n";
+	 // clrscr();
+	  cout<<"\n\n\n\n\n\n\t\t\t\tERROR YS3 MESS\n";
+	  cout<<"\n\n\t\t\t\t  CASH MEMO\n";
+	  cout<<"\n\nBILL NO.:"<<i;
+	  cout<<"\n\nNAME:";
+	  cout<<name;
+	  
+	  cout<<"\n\nSERVICE NO.:"<<a;
+	  
+	  cout<<"\n\nPHONE NO.:"<<c;
+	  cout<<"\n\n\n\n\n";
+	  cout<<"===============================================================================";
+	  cout<<"\nITEMS"            <<setw(40)<<"QUANTITY"<<setw(35)<<"PRICE(Rs.)";
+	  
+	  
+	  cout<<"\n===============================================================================";
+	  if(m>0)
+	    cout<<"\n\nnManchurian"        <<setw(30)<<m<<setw(32)<<"Rs."<<m*bir;
+	  if(n>0)
+	    cout<<"\n\nPani Puri"        <<setw(32)<<n<<setw(32)<<"Rs."<<n*per;
+	  if(o>0)
+	    cout<<"\n\nFried maggi"     <<setw(30)<<o<<setw(32)<<"Rs."<<o*der;
+	  if(p>0)
+	    cout<<"\n\nVeg cheese sandwich"            <<setw(22)<<p<<setw(32)<<"Rs."<<p*mur;
+	  if(q>0)
+	    cout<<"\n\nPav bhaji"<<setw(32)<<q<<setw(32)<<"Rs."<<q*par;
+	  if(r>0)
+	    cout<<"\n\nBhel puri" <<setw(32)<<r<<setw(32)<<"Rs."<<r*cor;
+	  if(s>0)
+	    cout<<"\n\nSamosa"      <<setw(35)<<s<<setw(32)<<"Rs."<<s*twr;
+	  if(t>0)
+	    cout<<"\n\nMix Fruit shake"         <<setw(26)<<t<<setw(32)<<"Rs."<<t*lur;
+	  if(u>0)
+	    cout<<"\n\nMisel pav"             <<setw(32)<<u<<setw(32)<<"Rs."<<u*tir;
+	  if(v>0)
+	    cout<<"\n\nChana dal vada"     <<setw(26)<<v<<setw(32)<<"Rs."<<v*rer;
+		e=m*bir;
+		f=n*per;
+		g=o*der;
+		h=p*mur;
+		k=q*par;
+		l=r*cor;
+		w=s*twr;
+		x=t*lur;
+		y=u*tir;
+		z=v*rer;
+		sum=e+f+g+h+k+l+w+x+y+z;
+		add=m+n+o+p+q+r+s+t+u+v;
+	    cout<<"\n===============================================================================";
+	    cout<<"\nTOTAL :"<<setw(36)<<add<<setw(30)<<"Rs."<<sum;
+	    cout<<"\n\nTOTALSNACKS FEES: "<<sum<<"/-ONLY";
+	    cout<<"\n\nTOTAL MONTHLY MESS FEES :"<<"Rs."<<4500+sum<<"/- ONLY";
+	    cout<<"\n\nTHANK YOU \t\t\t     VISIT US AGAIN\n";
+	getch();
+//	clrscr();
+	goto again;
+	}
+	else
+	{goto again;
+ 
+	}
+ 
+ 
+	case 2:
+	cout<<"\n\n\n\n\n\t\t\t\tCANTEEN DETAILS";
+	cout<<"\n\n#TIMING:\n\n 11 AM TO 02 PM \n 05 PM TO 10 PM";
+	cout<<"\n\n#CANTEEN WILL CLOSED ON SUNDAY ";
+	cout<<"\n\n#SNACKS ONCE SOLD WILL NOT BE EXCHANGED/REPLACED";
+	cout<<"\n\n#TAKE YOUR STUDENT ID CARD WITH YOU";
+	cout<<"\n\n#COLLECT YOUR STUDENT ID CARD & BILL AFTER PAYMENT";
+	cout<<"\n\n#DO NOT BREAK ANY CANTEEN ITEMS";
+	cout<<"\n\n#NO ONLINE PAYMENT OPTION AVAILABLE ONLY CASH PAYMENT AVAILABLE";
+	cout<<"\n\n#YOUR MONTHLY MESS FEES IS 4500 ";
+	cout<<"\n\n#THE MENU OF LUNCH AND DINNER ACCORDING TO DAYS IN A WEEK:\n 1.MONDAY\n 2.TUESDAY\n 3.WEDNESDAY\n 4.THRUSDAY\n 5.FRIDAY\n 6.SATURDAY";
+	cout<<"Enter your choice(1-6): ";
+	cin>>me;
+	switch(me)
+	{
+	 case 1:
+	 cout<<"LUNCH-Masoor dal tadka,Shahi paneer,Fried papad,rasam,Lemon rice"<<endl;
+	 cout<<"DINNER-Chilli gobi,Dal tadka,Curd,Sambhar,Jalebi"<<endl;
+	 break;
+	 case 2:
+	 cout<<"LUNCH-Soyabean curry,Seemb papdi dry,Dal tadka,Green apple"<<endl;
+	 cout<<"DINNER-Puri,Aloo tomato curry,Boiled peanuts"<<endl;
+	 break;
+	 case 3:
+	 cout<<"LUNCH-Curd rice,Moong dal,Banana fry,Rice papad,Rajma"<<endl;
+	 cout<<"DINNER-Idli vada,Curd rice,Chole masala,Gobi aloo"<<endl; 
+	 break;
+	 case 4:
+	 cout<<"LUNCH-Dal khichdi tadka,Curry pakoda,Long beans"<<endl;
+	 cout<<"DINNER-Chilli paneer,Mathi aloo,Plain masor,Besan ladoo,Rasam"<<endl;
+	 break;
+	 case 5:
+	 cout<<"LUNCH-Jeera rice,Masoor dal tadka,Dry bhindi"<<endl;
+	 cout<<"DINNER-Veg kholapuri,Plain toor dal,Sitafal rabdi"<<endl;
+	 break;
+	 case 6:
+	 cout<<"LUNCH-Chole Bhatore,Aloo jeera,Rasam,Kadi pakoda"<<endl;
+	 cout<<"DINNER-Masaledar bengan,Hyderabadi sambar,Orange,Gulab jamun"<<endl;
+	 break;
+
+	 default:
+	 cout<<"Wrong Choice"<<endl;
+}
+	getch();
+	//clrscr();
+	goto again;
+	case 3:
+	goto main;
+	case 4:
+	   if(m>0||n>0||o>0||p>0||q>0||r>0||s>0||t>0||u>0||v>0)
+	  {cout<<"\n\t\t\tItems Avaliable in Canteen.\n\n\n";
+	  cout<<"===============================================================================";
+	  cout<<"\nITEMS"            <<setw(42)<<"QUANTITY";
+	  cout<<"\n===============================================================================";
+	  cout<<"\n\n\nManchurian"        <<setw(35)<<abis;
+	  cout<<"\n\n\nPani puri"        <<setw(36)<<apep;
+	  cout<<"\n\n\nFried maggi"     <<setw(33)<<aden;
+	  cout<<"\n\n\nVeg cheese sandwich"            <<setw(26)<<amun;
+	  cout<<"\n\n\nPav bhaji"<<setw(35)<<aperk;
+	  cout<<"\n\n\nBhel puri" <<setw(35)<<acoc;
+	  cout<<"\n\n\n Samosa"      <<setw(42)<<atit;
+	  cout<<"\n\n\n Mix fruit shake"         <<setw(29)<<alux;
+	  cout<<"\n\n\nMisel pav"             <<setw(36)<<atid;
+	  cout<<"\n\n\nChana dal vada"     <<setw(30)<<aree;
+	  cout<<"\n*******************************************************************************";
+	  getch();
+	  goto again;
+	  }
+	  else
+	  {{cout<<"\n\t\t\tItems Avaliable in Canteen.\n\n\n";
+	  cout<<"===============================================================================";
+	  cout<<"\nITEMS"            <<setw(42)<<"QUANTITY";
+	  cout<<"\n===============================================================================";
+	  cout<<"\n\n\nManchurian"        <<setw(35)<<qubis;
+	  cout<<"\n\n\nPani puri"        <<setw(36)<<qupep;
+	  cout<<"\n\n\nFried maggi"     <<setw(33)<<quden;
+	  cout<<"\n\n\nVeg cheese sandwich"            <<setw(26)<<qumun;
+	  cout<<"\n\n\nPav bhaji"<<setw(35)<<quperk;
+	  cout<<"\n\n\nBhel puri" <<setw(35)<<qucoc;
+	  cout<<"\n\n\n Samosa"      <<setw(37)<<qutit;
+	  cout<<"\n\n\n Mix fruit shake"         <<setw(29)<<qulux;
+	  cout<<"\n\n\nMisel pav"             <<setw(36)<<qutid;
+	  cout<<"\n\n\nChana dal vada"     <<setw(30)<<quree;
+	  cout<<"\n\n*******************************************************************************";
+	  getch();
+	  goto again;
+ 
+	  }
+	case 5:
+	cout<<"\n\n\n\n\n\n\n\n\n\t\t\t\t   FOOD ";
+	sleep(1);
+//	clrscr();
+	cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t  ON  ";
+	sleep(1);
+//	clrscr();
+	cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t   YOUR ";
+	sleep(1);
+//	clrscr();
+	cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t   MIND ?? ";
+	sleep(1);
+//	clrscr();
+	cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t   SEE IT ";
+	sleep(1);
+//	clrscr();
+	cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t  ON YOUR ";
+	sleep(1);
+//	clrscr();
+	cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t   PLATE. ";
+	sleep(1);
+//	clrscr();
+	cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t  :) ";
+	sleep(1);
+//	clrscr();
+	cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t   THANKS\n\n\n\t\t\t\tPress any key to Exit.......";
+	getch();
+	exit(0);
+	default:
+		cout<<"Your choice is wrong";
+	getch();
+	goto again;
+	 }
+ //getch();
+ //return 0;
+ }
+ 
+}
+
+    
+
